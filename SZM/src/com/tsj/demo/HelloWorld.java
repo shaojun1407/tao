a8fae76fd8cb804021605e9a4bcf27e1c852c26c
lenovo <shaojun1209438958@qq.com>
2017-08-22 10:54:08 +0800

    add operator tao

--- SZM/src/com/tsj/demo/HelloWorld.java
+++ SZM/src/com/tsj/demo/HelloWorld.java
@@ -3,8 +3,17 @@ package com.tsj.demo;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello,World");
-
+		int i = 127;
+		byte j=(byte) i;
+		System.out.println("i = "+i);
+		System.out.println("j = "+j);
+		byte k=10;
+		System.out.println("k ="+k);
+		i=k;//大转小强制转换，小转大自行转换
+		System.out.println("i = "+i);
+		
+		int a = 8;
+		a /= 4;//a= a/4
+		System.out.println("a /= 4: " +a);
 	}
-
 }

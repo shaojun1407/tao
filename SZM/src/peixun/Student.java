62df5dc1f19c9b43e626ff548d42c9edc4686df0
lenovo <shaojun1209438958@qq.com>
2017-08-24 17:54:26 +0800

    add commit

---
+++ SZM/src/peixun/Student.java
@@ -0,0 +1,11 @@
+package peixun;
+
+public class Student {
+String name;
+int age;
+int classNo;
+String hobby;
+public void show() {
+	System.out.println("姓名:"+name+"\n 年龄:"+age+"\n 班级编号:"+classNo+"\n 爱好:"+hobby);
+}
+}
---
+++ SZM/src/peixun/TestStudent.java
@@ -0,0 +1,16 @@
+package peixun;
+
+public class TestStudent {
+
+	public static void main(String[] args) {
+		Student student =new Student();
+		student.name ="zhangsan";
+		student.age = 25;
+		student.classNo=412;
+		student.hobby ="跆拳道";
+		student.show();
+		
+
+	}
+
+}

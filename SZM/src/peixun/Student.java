27d25615f10cefb5ccccbaf04e28e85eeb78398d
tao <shaojun1209438958@qq.com>
2017-08-30 10:10:11 +0800

    new add

--- SZM/src/peixun/Student.java
+++ SZM/src/peixun/Student.java
@@ -1,11 +1,14 @@
 package peixun;
 
+import lombok.Getter;
+
+@Getter
 public class Student {
-String name;
-int age;
-int classNo;
-String hobby;
-public void show() {
-	System.out.println("姓名:"+name+"\n 年龄:"+age+"\n 班级编号:"+classNo+"\n 爱好:"+hobby);
-}
+	String name;
+	int age;
+	int classNo;
+	String hobby;
+	public void show() {
+		System.out.println("姓名:" + name + "\n 年龄:" + age + "\n 班级编号:" + classNo + "\n 爱好:" + hobby);
+	}
 }

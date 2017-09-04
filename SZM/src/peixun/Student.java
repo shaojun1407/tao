500a662c0fb8d87afc60a967dbad56db5f161d85
lenovo <shaojun1209438958@qq.com>
2017-09-04 20:49:18 +0800

    add new 04

--- SZM/src/peixun/Student.java
+++ SZM/src/peixun/Student.java
@@ -1,11 +1,18 @@
 package peixun;
 
 public class Student {
-String name;
-int age;
-int classNo;
-String hobby;
-public void show() {
-	System.out.println("姓名:"+name+"\n 年龄:"+age+"\n 班级编号:"+classNo+"\n 爱好:"+hobby);
-}
+//String name;
+//int age;
+//int classNo;
+//String hobby;
+//public void show() {
+//	System.out.println("姓名:"+name+"\n 年龄:"+age+"\n 班级编号:"+classNo+"\n 爱好:"+hobby);
+//}
+	String name;
+	int age;
+	String classNo;
+	String hobby;
+	public void show() {
+		System.out.println("姓名: "+name+"班级号: "+classNo+"爱好: "+hobby);
+	}
 }
--- SZM/src/peixun/TestpPenguin.java
+++ SZM/src/peixun/TestpPenguin.java
@@ -1,6 +1,6 @@
-package com.tsj.demo;
+package peixun;
 
-public class selfAddMinus {
+public class TestpPenguin {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
